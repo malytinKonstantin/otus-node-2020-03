4937369fa21f6c397294e37ddc0d11e67b5074eb
const_malytin <malyytin@gmail.com>
2020-05-12 19:23:42 +0300

    fix changees

--- 02-module-system-in-nodejs/src/print-tree/getDirTree.spec.js
+++ 02-module-system-in-nodejs/src/print-tree/getDirTree.spec.js
@@ -2,15 +2,18 @@ const mock = require('mock-fs')
 const path = require('path')
 const getDirTree = require('./getDirTree')
 
-const folder = 'fake-folder'
+const folder1 = 'fake-folder1'
+const folder2 = 'fake-folder2'
 const items = {
   'file1.js': '',
   'file2.js': '',
 }
 
 beforeEach(() => {
-   mock({
-    [folder]: mock.directory({ items })
+  mock({
+    [`${folder1}/${folder2}`]: mock.directory({
+      items
+    })
   })
 })
 
@@ -19,17 +22,19 @@ afterEach(mock.restore)
 describe('test print-tree', () => {
     it('shoud print tree', async () => {
         const rootDir = process.cwd()
-        const depth = 1
+        const depth = 3
         const tree = await getDirTree(rootDir, depth)
         const files = Object.keys(items)
         
         expect(tree.length).toBe(1)
-        expect(tree[0].name).toBe(folder)
-        expect(tree[0].path).toBe(path.resolve(rootDir, folder))
-        expect(tree[0].children.length).toBe(2)
-        expect(tree[0].children[0].name).toBe(files[0])
-        expect(tree[0].children[0].path).toBe(path.resolve(rootDir, folder, files[0]))
-        expect(tree[0].children[1].name).toBe(files[1])
-        expect(tree[0].children[1].path).toBe(path.resolve(rootDir, folder, files[1]))
+        expect(tree[0].name).toBe(folder1)
+        expect(tree[0].path).toBe(path.resolve(rootDir, folder1))
+        expect(tree[0].children.length).toBe(1)
+        
+        expect(tree[0].children[0].children.length).toBe(2)
+        expect(tree[0].children[0].children[0].name).toBe(files[0])
+        expect(tree[0].children[0].children[0].path).toBe(path.resolve(rootDir, folder1, folder2, files[0]))
+        expect(tree[0].children[0].children[1].name).toBe(files[1])
+        expect(tree[0].children[0].children[1].path).toBe(path.resolve(rootDir, folder1, folder2, files[1]))
     })
 })
